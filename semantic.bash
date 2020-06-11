4b30e90fea9991f97ddc27e60f5f0d4a122379d4
zytzrh <zytzrh@outlook.com>
2020-06-11 20:17:40 +0800

    modify bash

--- semantic.bash
+++ semantic.bash
@@ -1,6 +1,6 @@
 set -e
 cd "$(dirname "$0")"
 cat > basic-2.mx
-#export CCHK="java -classpath /ulib/java/antlr-4.8-complete.jar:./bin Main semantic"
-export CCHK="java -classpath /mnt/d/Code/Compiler/MXCompiler/Antlr/antlr-4.8-complete.jar:./bin main semantic"
+export CCHK="java -classpath /ulib/java/antlr-4.8-complete.jar:./bin Main semantic"
+#export CCHK="java -classpath /mnt/d/Code/Compiler/MXCompiler/Antlr/antlr-4.8-complete.jar:./bin main semantic"
 $CCHK
